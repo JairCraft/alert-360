2154b7a8b71d5ed3f72b0f27edc8d567dac6b008
Jair Guanotoa <jairguanotoa@hotmail.com>
2025-02-19 08:34:02 -0500

    build: :construction: WIP firebase notifixations

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -1,17 +1,15 @@
-import { initializeApp } from "@react-native-firebase/app";
-import { getMessaging } from "@react-native-firebase/messaging";
+import firebase from "@react-native-firebase/app";
+import "@react-native-firebase/messaging";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCETRcJ0zPpmOo2l7av30R4CkdsCx9wgj4",
-  authDomain: "",
   projectId: "alert360-c580b",
   storageBucket: "alert360-c580b.firebasestorage.app",
   messagingSenderId: "943921982787",
   appId: "1:943921982787:android:b5a24cc1fa588d2a9f93f6",
 };
 
-const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
-messaging;
+const app = await firebase.initializeApp(firebaseConfig);
+const messaging = app.messaging();
 
 export { messaging };
