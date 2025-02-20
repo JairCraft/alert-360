8cfd28a345d5b65b32af5abf5f34d31ecdb4bf25
Jair Guanotoa <jairguanotoa@hotmail.com>
2025-02-19 19:29:25 -0500

    feat: :sparkles: Implemented push notifications

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import MainNavigator from "./MainNavigator";
 import Toast from "react-native-toast-message";
-import { Platform } from "react-native";
 import messaging from "@react-native-firebase/messaging";
 import { PermissionsAndroid } from "react-native";
 
@@ -10,44 +9,30 @@ export default function App() {
   useEffect(() => {
     const requestUserPermission = async () => {
       PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS)
-      console.log('hola');
 
-      await messaging().registerDeviceForRemoteMessages()
-      console.log('adios');
+      const authStatus = await messaging().requestPermission()
 
-      //const enabled = authStatus === messaging.AuthorizationStatus.AUTHORIZED || authStatus === messaging.AuthorizationStatus.PROVISIONAL
+      const enabled = authStatus === messaging.AuthorizationStatus.AUTHORIZED || authStatus === messaging.AuthorizationStatus.PROVISIONAL
 
-      /* if (enabled) {
+      if (enabled) {
         console.log('Auth status ' + authStatus);
-      } */
-      const token = await messaging().getToken()
-      console.log('FCM token: ' + token);
+        await messaging().registerDeviceForRemoteMessages()
+        const token = await messaging().getToken()
+        console.log('FCM token: ' + token);
+      }
     }
-    /* 
-        const message = await messaging().getInitialNotification();
-    
-        const unsubscribe = messaging().onNotificationOpenedApp(remoteMessage => {
-    
-        }); */
-    console.log('useEfeect');
 
     requestUserPermission()
-    console.log('useEfeect pass');
 
   }, [])
-  /* 
-    messaging().setBackgroundMessageHandler(async remoteMessage => {
-      console.log('Message handled in the background!', remoteMessage);
-    });
-  
-    const foreground = messaging().onMessage(async remoteMessage => {
-      console.log('A new FCM message arrived!', remoteMessage);
-  
-    });
-  
-    foreground(); */
 
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      console.log("FCM Message received in foreground:", remoteMessage);
+    });
 
+    return unsubscribe;
+  }, []);
 
   return (
     <>
